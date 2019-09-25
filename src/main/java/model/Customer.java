ef16411b193d51b0cb8c469d3db166232b2bddee
Seweryn91 <s.talaj@gmail.com>
2019-09-25 13:26:25 +0200

    Implement class Customer

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Customer {
+
+    private int id;
+    private String name;
+    private String city;
+    private String zipCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
