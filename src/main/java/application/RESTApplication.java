5bc14f5fb5590d0e6b216c37536f371b3b0ad988
Seweryn91 <s.talaj@gmail.com>
2019-09-25 14:17:09 +0200

    Add Tomcat dependency, move Main Application class to anther package

---
+++ src/main/java/application/RESTApplication.java
@@ -0,0 +1,12 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RESTApplication.class, args);
+    }
+}
