89c6f3cf9d11d039beda2e2f9e9db7a7ce5d2cf0
Seweryn91 <s.talaj@gmail.com>
2019-09-25 16:43:09 +0200

    Add ComponentScan annotation

--- src/main/java/application/RESTApplication.java
+++ src/main/java/application/RESTApplication.java
@@ -1,7 +1,11 @@
+package application;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"model", "service"})
 public class RESTApplication {
 
     public static void main(String[] args) {
